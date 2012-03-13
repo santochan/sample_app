fb4d26aaa25abc1179a6de267c626d32aa2e79f8
santochan <last1475@126.com>
2012-03-13 10:52:10 +0800

    change styles and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,12 +4,12 @@ describe "StaticPages" do
   
   describe "Home Page" do
     it "should have the h1 'Sample App'" do
-      visit "/static_pages/home"
+      visit root_path
       page.should have_selector('h1',:text => 'Sample App')
     end
     
     it "should have the title 'Home'" do
-      visit "/static_pages/home"
+      visit root_path
       page.should have_selector('title',
                   :text  => "Ruby on Rails Tutorial Sample App | Home")
     end
@@ -18,12 +18,12 @@ describe "StaticPages" do
   
   describe "Help Page" do
     it "should have the h1 'Help'" do
-      visit "/static_pages/help"
+      visit help_path
       page.should have_selector('h1',:text => 'Help')
     end
     
     it "should have the title 'Help'" do
-      visit "/static_pages/help"
+      visit help_path
       page.should have_selector('title',
                   :text  => "Ruby on Rails Tutorial Sample App | Help")
     end
@@ -31,15 +31,29 @@ describe "StaticPages" do
   
   describe "About page" do
     it "should have the h1 'About us'" do
-      visit "/static_pages/about"
+      visit about_path
       page.should have_selector('h1',:text => 'About us')
     end
     
     it "should have the title 'About us'" do
-      visit "/static_pages/about"
+      visit about_path
       page.should have_selector('title',
                   :text  => "Ruby on Rails Tutorial Sample App | About us")
     end
+  end
+  
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: "Contact")
+    end
+    
+    it "should have the title 'Contact'"do
+      visit contact_path
+      page.should have_selector('title',
+                  text: "Ruby on Rails Tutorial Sample App | Contact")
+    end
+    
   end
   
 end
