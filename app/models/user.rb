8f66e1b8b183f3f82ae0771fd9667c86bc360112
santochan <last1475@126.com>
2012-03-20 13:46:22 +0800

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name,:email,:password,:password_confirmation
   has_secure_password
+  before_save :create_remember_token
   
   validates :name,  presence:true, length: { maximum: 50}
   
@@ -21,6 +22,13 @@ class User < ActiveRecord::Base
             
   validates :password, length:{ minimum: 6}
   validates :password_confirmation, presence:true
+  
+  
+  private
+    
+    def create_remember_token
+      self.remember_token= SecureRandom.urlsafe_base64
+    end
             
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new,:create,:destory]
 
   root to: 'static_pages#home'
   match '/help', to: 'static_pages#help'
@@ -7,7 +8,9 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
   match '/', to: 'static_pages#home'
   
-  match '/signup',to: 'users#new'
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
